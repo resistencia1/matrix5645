a3fd8e01dbb0bf0eda4f4353a4d98ff131b554f3
resistencia1 <crisevil1@gmail.com>
2020-08-11 20:25:45 -0500

    rewr

--- app/src/main/java/com/example/matrix5645/MainActivity.java
+++ app/src/main/java/com/example/matrix5645/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.matrix5645;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.animation.Animation;
@@ -17,8 +20,18 @@ private Toolbar toolbar;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        CoordinatorLayout coordinatorLayout = findViewById(R.id.layout);
+//        AnimationDrawable animationDrawable = (AnimationDrawable) coordinatorLayout.getBackground();
+//        animationDrawable.setEnterFadeDuration(2000);
+//        animationDrawable.setExitFadeDuration(4000);
+//        animationDrawable.start();
+
         toolbar = findViewById(R.id.mCustomToolbar);
         setSupportActionBar(toolbar);
+        AnimationDrawable animationDrawable1 = (AnimationDrawable) toolbar.getBackground();
+        animationDrawable1.setEnterFadeDuration(2000);
+        animationDrawable1.setExitFadeDuration(4000);
+        animationDrawable1.start();
         getSupportActionBar().setTitle("El Botcito Manco");
         getSupportActionBar().setIcon(getDrawable(R.drawable.botz1));
 
